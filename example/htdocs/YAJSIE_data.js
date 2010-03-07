38eead21aeb0f333554c6cd0f589602b9274a534
wiese <github@tschagrass.de>
2010-03-07 18:07:14 +0100

    first steps. mostly the JS side

---
+++ example/htdocs/YAJSIE_data.js
@@ -0,0 +1,17 @@
+var YAJSIE_data = {
+	'de_DE' : {
+		"pluralForms": "nplurals=2; plural=n != 1;",
+		"template": {
+			"pattern": /(^|.|\r|\n)(#(\w+)#)/
+		},
+		"translations": {
+			"i am the first text": "ich bin der erste text",
+			"me beeing the second test": "ich bin der zweite text",
+			"#n# day ago": {
+				"msgid_plural": "#n# days ago",
+				"msgstr[0]": "vor #n# Tag",
+				"msgstr[1]": "vor #n# Tagen"
+			}
+		}
+	}
+};
