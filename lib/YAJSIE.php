fdc4c52bb71d5f9e00a521fea40dc9784f2a7950
wiese <github@tschagrass.de>
2010-03-09 21:41:15 +0100

    more tests, created debug var and get method, sketch php

---
+++ lib/YAJSIE.php
@@ -0,0 +1,22 @@
+<?php
+
+class YAJSIE {
+
+	protected $localeDir;
+
+	// get po files in all sub directories
+
+	// get (first level) sub directory names
+
+	// concat all po files ?
+
+	// extract all translations
+
+	// convert translations (charset given in file) to utf-8
+
+	// store json file as utf-8
+
+	//
+}
+
+?>
